466891bed8f8f07921f08fec73adad8520a21a64
Prabhjeet Singh <prabhijeetsidhu@gmail.com>
2023-03-14 18:26:00 -0400

    image replace

--- todo-app-main/src/App.js
+++ todo-app-main/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import bgDesktopImg from './images/bg-desktop-dark.jpg';
+import light from './images/icon-sun.svg';
+import dark from './images/icon-moon.svg';
+import { useState } from 'react';
 
 function App() {
+  const [lightMode,useLightMode]=useState(true)
   return (
-    <div className="App " >
-    
-    </div>
+    <>
+    <div className="App h-screen bg-black   " >
+     <div className='flex justify-between items-center z-40 relative '>
+      <h1>todo</h1>
+      <img src={lightMode?light:dark} alt='mode' />
+     </div>
+        </div>
+     <img src={bgDesktopImg} alt="img" className=' w-screen absolute  top-0'/>
+    </>
   );
 }
 
