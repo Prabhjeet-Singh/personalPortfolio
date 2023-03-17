6d4ff15128c8c2cf34e91bf49e4be1393e7da51c
Prabhjeet Singh <prabhijeetsidhu@gmail.com>
2023-03-16 23:18:46 -0400

    setting up background

--- todo-app-main/src/App.js
+++ todo-app-main/src/App.js
@@ -1,21 +1,37 @@
 import './App.css';
 import bgDesktopImg from './images/bg-desktop-dark.jpg';
+import bgLightDesktopImg from './images/bg-desktop-light.jpg';
 import light from './images/icon-sun.svg';
 import dark from './images/icon-moon.svg';
 import { useState } from 'react';
 
 function App() {
-  const [lightMode,useLightMode]=useState(true)
+  const [lightMode,setLightMode]=useState(true)
+  const [name ,setName]=useState('')
   return (
-    <>
-    <div className="App h-screen bg-black   " >
-     <div className='flex justify-between items-center z-40 relative '>
-      <h1>todo</h1>
-      <img src={lightMode?light:dark} alt='mode' />
-     </div>
+    < div className=' flex flex-col h-screen bg-black items-center justify-center'>
+     <img src={lightMode?bgLightDesktopImg:bgDesktopImg} alt="img" className=' w-screen absolute  top-0 '/>
+    
+  <div className='z-10 max-w-[60rem] '>
+    
+        
+      {/* navbar start */}
+        <div className='flex justify-between items-center   max-w-[40rem]'>
+          <h1 className='uppercase text-4xl'>todo</h1>
+          <img src={lightMode?light:dark} alt='mode' onClick={()=>setLightMode(!lightMode)}/>
         </div>
-     <img src={bgDesktopImg} alt="img" className=' w-screen absolute  top-0'/>
-    </>
+       
+        {/* navbar end */}
+
+        <input type="text"  value={name} onChange={(e)=>{setName(e.target.value)}} className=''/>
+      
+   
+    {console.log(name)}
+  </div>
+      
+
+
+    </div>
   );
 }
 
