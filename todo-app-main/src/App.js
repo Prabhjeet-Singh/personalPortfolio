8566621dfcf9d8c46cd11dd484abc6855476357b
Prabhjeet Singh <prabhijeetsidhu@gmail.com>
2023-03-20 21:19:02 -0400

    add body file

--- todo-app-main/src/App.js
+++ todo-app-main/src/App.js
@@ -1,35 +1,45 @@
-import './App.css';
-import bgDesktopImg from './images/bg-desktop-dark.jpg';
-import bgLightDesktopImg from './images/bg-desktop-light.jpg';
-import light from './images/icon-sun.svg';
-import dark from './images/icon-moon.svg';
-import { useState } from 'react';
+import "./App.css";
+import bgDesktopImg from "./images/bg-desktop-dark.jpg";
+import bgLightDesktopImg from "./images/bg-desktop-light.jpg";
+import light from "./images/icon-sun.svg";
+import dark from "./images/icon-moon.svg";
+import { useState } from "react";
+import Body from "./components/Body";
 
 function App() {
-  const [lightMode,setLightMode]=useState(true)
-  const [name ,setName]=useState('')
+  const [lightMode, setLightMode] = useState(true);
+  const [name, setName] = useState("");
   return (
-    < div className=' flex flex-col h-screen bg-black items-center justify-center'>
-     <img src={lightMode?bgLightDesktopImg:bgDesktopImg} alt="img" className=' w-screen absolute  top-0 '/>
-    
-  <div className='z-10 max-w-[60rem] '>
-    
-        
-      {/* navbar start */}
-        <div className='flex justify-between items-center   max-w-[40rem]'>
-          <h1 className='uppercase text-4xl'>todo</h1>
-          <img src={lightMode?light:dark} alt='mode' onClick={()=>setLightMode(!lightMode)}/>
+    <div className=" flex flex-col h-screen bg-black items-center justify-center text-white">
+      <img
+        src={lightMode ? bgLightDesktopImg : bgDesktopImg}
+        alt="img"
+        className=" w-screen absolute  top-0 "
+      />
+
+      <div className="z-10 max-w-[60rem] ">
+        {/* navbar start */}
+        <div className="flex justify-between items-center   max-w-[40rem]">
+          <h1 className="uppercase text-4xl text-white">todo</h1>
+          <img
+            src={lightMode ? light : dark}
+            alt="mode"
+            onClick={() => setLightMode(!lightMode)}
+          />
         </div>
-       
+
         {/* navbar end */}
 
-        <input type="text"  value={name} onChange={(e)=>{setName(e.target.value)}} className=''/>
-      
-   
-  </div>
-      
-
-
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          className=""
+        />
+      </div>
+      <Body/>
     </div>
   );
 }
--- todo-app-main/src/components/body.js
+++ todo-app-main/src/components/body.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-const body = () => {
-  return <div>body</div>;
+const Body = () => {
+  return <div className="min-w-[40vw] min-h-[30vh] bg-red-300  z-10 flex flex-col justify-between ">
+    <span className="self-center">list</span>
+    <div className="grid grid-cols-3">
+      <span>items left</span>
+      <div className="grid gap-2 grid-cols-3">
+        <button className="">All</button>
+        <button className="">Active</button>
+        <button className="">Completed</button>
+       
+      </div>
+      <button  className="capitalize">clear Completed</button>
+    </div>
+    </div>;
 };
 
-export default body;
+export default Body;
