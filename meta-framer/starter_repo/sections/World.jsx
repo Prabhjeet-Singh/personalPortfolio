3822256a913a8dfef63ddb1c5ef6f8c0a50e8cda
Prabhjeet Singh <prabhijeetsidhu@gmail.com>
2023-06-09 16:53:17 -0400

    _Write the insight file

--- meta-framer/starter_repo/sections/World.jsx
+++ meta-framer/starter_repo/sections/World.jsx
@@ -12,7 +12,7 @@ const World = () => (
   initial="hidden"
   whileInView="show"
   viewport={{once:'false',amount:0.25}}
-  className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+  className={`${styles.innerWidth} mx-auto flex  flex-col `}
   >
     <TypingText title="| People on the  World"
     textSytles="text-center"
@@ -25,6 +25,37 @@ const World = () => (
     )}
     textStyles="text-center"
     />
+    <motion.div
+    variants={fadeIn('up',"tween,0.3,1")}
+    className="relative mt-[69px] flex w-full h-[500px]"
+    >
+      <img
+       src="/map.png"
+       alit="map"
+       className="w-full h-full object-cover"
+      />
+      <div
+      className="rounded-full absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] bg-[#5d6680]"
+      >
+        <img src="people-01.png" alt="people"
+        className="w-full h-full"
+        />
+      </div>
+      <div
+      className="rounded-full absolute top-10 left-20 w-[70px] h-[70px] p-[6px] bg-[#5d6680]"
+      >
+        <img src="people-02.png" alt="people"
+        className="w-full h-full"
+        />
+      </div>
+      <div
+      className="rounded-full absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] bg-[#5d6680]"
+      >
+        <img src="people-03.png" alt="people"
+        className="w-full h-full"
+        />
+      </div>
+    </motion.div>
   </motion.div>
   </section>
 );
