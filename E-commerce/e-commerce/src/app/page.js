0a3e7d38fd40c330674c14639fd1e5a325389d09
Prabhjeet Singh <prabhijeetsidhu@gmail.com>
2023-07-14 23:18:05 -0400

    updating

--- E-commerce/e-commerce/src/app/page.js
+++ E-commerce/e-commerce/src/app/page.js
@@ -1,30 +1,52 @@
-export default function Home({ pet }) {
+import { createClient } from "next-sanity";
+export default function Home({ pets }) {
   return (
-    <main className="">
-      <h2>pets</h2>
-      {pet.length > 0 && (
-        <ul>
-          {pet.map((pet) => (
-            <li key={pet._id}>{pet?.name}</li>
-          ))}
-        </ul>
-      )}
-      {!pet.length > 0 && <p>No pets to show</p>}
-      {pet.length > 0 && (
-        <div>
-          <pre>{JSON.stringify(pets, null, 2)}</pre>
-        </div>
-      )}
-      {!pet.length > 0 && (
-        <div>
-          <div>¯\_(ツ)_/¯</div>
-          <p>
-            Your data will show up here when you've configured everything
-            correctly
-          </p>
-        </div>
-      )}
-      {/* {pets.length>0 ? pets.map((pet)=> ( pet.name)) : "String is empty" } */}
-    </main>
+    <>
+      <header>
+        <h1>Sanity + Next.js</h1>
+      </header>
+      <main>
+        <h2>pets</h2>
+        {pets.length > 0 && (
+          <ul>
+            {pets.map((pet) => (
+              <li key={pet._id}>{pet?.name}</li>
+            ))}
+          </ul>
+        )}
+        {!pets.length > 0 && <p>No pets to show</p>}
+        {pets.length > 0 && (
+          <div>
+            <pre>{JSON.stringify(pets, null, 2)}</pre>
+          </div>
+        )}
+        {!pets.length > 0 && (
+          <div>
+            <div>¯\_(ツ)_/¯</div>
+            <p>
+              Your data will show up here when you've configured everything
+              correctly
+            </p>
+          </div>
+        )}
+      </main>
+    </>
   );
+}
+
+const client = createClient({
+  projectId: "ao302kp2",
+  dataset: "production",
+  apiVersion: "2023-7-14",
+  useCdn: false
+});
+
+export async function getStaticProps() {
+  const pets = await client.fetch(`*[_type == "pet"]`);
+
+  return {
+    props: {
+      pets
+    }
+  };
 }
