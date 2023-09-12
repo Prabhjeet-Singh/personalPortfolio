2453812fc6a422178cd80a4319fcff3042c1395e
Prabhjeet Singh <prabhijeetsidhu@gmail.com>
2023-09-12 16:27:31 -0400

    horizontal scrolling -f

--- E-commerce/src/components/AllProducts.jsx
+++ E-commerce/src/components/AllProducts.jsx
@@ -7,9 +7,25 @@ const AllProduts = ({hardware,clothes,electronics}) => {
     {/* this is 100vh of webpage */}
     <ShopNow/>
     <div>
-      <h2>Hardware Items</h2>
+      <h2 className="text-xl text-pink-500 p-4">Hardware Items</h2>
       <div className="flex overflow-x-scroll scroll-m-1">
      {hardware.map((item)=>{
+       return (<CateProducts item={item}/>);
+      })}
+      </div>
+    </div>
+    <div>
+      <h2 className="text-xl text-pink-500 p-4">Clothes</h2>
+      <div className="flex overflow-x-scroll scroll-m-1">
+     {clothes.map((item)=>{
+       return (<CateProducts item={item}/>);
+      })}
+      </div>
+    </div>
+    <div>
+      <h2 className="text-xl text-pink-500 p-4">Electronics</h2>
+      <div className="flex overflow-x-scroll scroll-m-1">
+     {electronics.map((item)=>{
        return (<CateProducts item={item}/>);
       })}
       </div>
--- E-commerce/src/components/semiComponents/CateProducts.jsx
+++ E-commerce/src/components/semiComponents/CateProducts.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const CateProducts = ({item}) => {
-  return (<div className="min-w-[15rem] min-h-[10rem] bg-white shadow-lg rounded-xl m-3 flex items-center justify-center">
+  return (<div className="min-w-[20rem] min-h-[15rem] bg-white shadow-lg rounded-xl m-3 flex items-center justify-center">
     <img src={item.imageUrl} alt={item.title} className='w-[6rem] h-[6em] ' />
     <div>
     <h2>{item.title}</h2>
